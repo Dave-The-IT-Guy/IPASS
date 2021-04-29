750600db1fcf65b858fd685a497b62911e41dbb4
Dave-The-IT-Guy <davevanderleek@gmail.com>
2021-04-29 22:05:29 +0200

    Basis opzet keuzemenu en vertaalt naar engels

--- Logbrowser/Logbrowser.py
+++ Logbrowser/Logbrowser.py
@@ -1,34 +1,33 @@
 ############################################################
-#Naam: Dave van der Leek (1777075)
-#Klas:
-#Programanaam: OVPNLogbrowser
-#Beschrijving: Een programma om OpenVPN logfiles uit te lezen
-#Versie:
-versie = 0.1
+#Name: Dave van der Leek (1777075)
+#Class:
+#Programname: OVPNLogbrowser
+#Description: Een programma om OpenVPN logfiles uit te lezen
+version = 0.1
 ############################################################
 
-#Functies
+#Functions
 
-#Functie die het menu laat zien
-def dvdl_toon_menu():
-    #Laat het menu zien:
-    #Wat wilt u doen?
-    #1. Top 10 IP's met de meeste unsuccesvolle verbindingen
-    #2. Top 10 IP's met de meeste succesvolle verbindingen
-    #3. Top 10 succesvolle verbindingen per adres
-    #4. Top 10 unsuccesvolle verbindingen per adres
-    #5. Top 5 dagen met veel succesvolle connecties
-    #6. Top 5 Dagen met veel unsuccesvolle connecties
-    #7. Laat aantal verbindingen zien die niet met OVPN gemaakt zijn
-    #8. Laat gebruikte management commando's zien (gesorteerd op aantal)
-    #9. Type een adres in en laat aantal connecties zien (en of ze gelukt zijn)
-    #10. Laat alle nieuwe IP's zien
-    #11. Maak een configfile aan
-    #x12. Laat parameters zien
-    #13. Sluit programma af
-    #Maak uw keuze:
-    print("Wat wilt u doen?\n1. Ik wil ...\n2. Ik wil...")
+#This functions shows the menu
+def dvdl_show_menu():
+    print("How may I assist you?\n"
+          "1.  Show me the top 10 IP's with the most unsuccessful connections\n"
+          "2.  Show me the top 10 IP's with most successful connections\n"
+          "3.  Show me the top 5 days with the most unsuccessful connections\n"
+          "4.  Show me the top 5 days with the most successful connections\n"
+          "5.  Show me how many connections weren't made with OpenVPN\n"
+          "6.  Show me all used management commando's\n"
+          "7.  I want to check an IP-adres for attempted connection's\n"
+          "8.  Show me all new IP-adresses\n"
+          "9.  I want to create a new configuration file\n"
+          "10. Show me all the program parameters\n"
+          "11. Close program\n")
+    #Ask which option needs to be selected
+    choice = input("Make your choice: ")
 
-    #Vraag om input en sla deze op in een variabele
-    choice = input("Maak uw keuze: ")
-dvdl_toon_menu()
+#At boot code
+#TODO: Check if program is interactive or not
+#TODO: Check if the logfile exists
+#TODO: Read entire log file and put it in a dictonairy?
+
+dvdl_show_menu()
