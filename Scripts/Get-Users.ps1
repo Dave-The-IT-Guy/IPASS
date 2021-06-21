4ebab0798072d8ef38cc566095b621fcc4e8b6da
Dave-The-IT-Guy <davevanderleek@gmail.com>
2021-06-21 20:08:52 +0200

    Made some changes

---
+++ Scripts/Get-Users.ps1
@@ -0,0 +1,15 @@
+﻿$location = "S:\users.txt"
+
+Try {
+    Remove-Item -Path $location -Force | Out-Null
+}
+
+Catch {}
+
+$AccountNames = Get-AdGroupMember -Identity Ubuntu
+
+foreach($Account in $AccountNames) {
+    If ($Account.objectClass -eq "user") {
+        Out-File $location -InputObject $Account.SamAccountName -Encoding ascii -Append
+    }
+}
